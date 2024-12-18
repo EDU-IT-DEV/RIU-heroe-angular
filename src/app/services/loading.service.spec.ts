885f74ea63e51b177dd682ce604920ce41614f2b
Eduardo Munoz <eduardo.munoz@globallogic.com>
2024-12-18 02:29:18 -0300

    Unit tests are implemented for the loading service

--- src/app/services/loading.service.spec.ts
+++ src/app/services/loading.service.spec.ts
@@ -1,14 +1,51 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { LoadingService } from './loading.service';
 
-describe('Service: Loading', () => {
+describe('LoadingService', () => {
+  let service: LoadingService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [LoadingService]
     });
+
+    service = TestBed.inject(LoadingService);
   });
 
-  it('should ...', inject([LoadingService], (service: LoadingService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should initialize loading signal with false', () => {
+    expect(service.loading()).toBeFalse();
+  });
+
+  it('should set loading to true', () => {
+    const expectedValue = true;
+
+    service.setLoading(expectedValue);
+
+    expect(service.loading()).toBeTrue();
+  });
+
+  it('should set loading to false', () => {
+
+    const expectedValue = false;
+    service.setLoading(true);
+
+    service.setLoading(expectedValue);
+
+    expect(service.loading()).toBeFalse();
+  });
+
+  it('should toggle loading state correctly', () => {
+    service.setLoading(true);
+    expect(service.loading()).toBeTrue();
+
+    service.setLoading(false);
+    expect(service.loading()).toBeFalse();
+
+    service.setLoading(true);
+    expect(service.loading()).toBeTrue();
+  });
 });
